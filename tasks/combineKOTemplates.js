2d754d4182d5f49ec67b388183fa9c811019a275
Jonathan Horowitz <jhorowitz@firedrum.com>
2017-04-06 15:13:02 -0700

    Support loading Mosaico after the DOMContentLoaded event is fired.

--- tasks/combineKOTemplates.js
+++ tasks/combineKOTemplates.js
@@ -5,11 +5,12 @@ module.exports = function(grunt) {
   grunt.registerMultiTask("combineKOTemplates", function() {
     var files = grunt.file.expand(this.data.src),
         result = "";
- 
+
     var script = this.data.templateSystemPath || '../src/js/bindings/choose-template.js';
 
+    result += "var $ = require('jquery');\n";
     result += "var templateSystem = require('"+script+"');\n";
-    result += "document.addEventListener('DOMContentLoaded', function(event) {\n";
+    result += "$(document).ready(function() {\n";
     files.forEach(function(file) {
       //strip the extension to determine a template name
       var name = path.basename(file).replace(".tmpl.html", ""),
