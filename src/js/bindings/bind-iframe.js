a2f3608e738b8d60602736fc18031c46aab0bdbe
Jonathan Horowitz <jhorowitz@firedrum.com>
2017-04-06 15:52:02 -0700

    Fix iFrame binding causing images to sometimes disappear in the preview pane and/or editor pane.

--- src/js/bindings/bind-iframe.js
+++ src/js/bindings/bind-iframe.js
@@ -20,42 +20,55 @@ ko.bindingHandlers.bindIframe = {
   tpl: "<!DOCTYPE html>\r\n<html>\r\n<head>\r\n</head>\r\n<body><div data-bind=\"block: content\"></div></body>\r\n</html>\r\n",
   init: function(element, valueAccessor) {
     function bindIframe(local) {
-      try {
-        var iframe = element.contentDocument;
-        iframe.open();
-        iframe.write(ko.bindingHandlers.bindIframe.tpl);
-        iframe.close();
+        var finishBind = function() {
+            try {
+                var iframe = element.contentDocument;
+                iframe.open();
+                iframe.write(ko.bindingHandlers.bindIframe.tpl);
+                iframe.close();
 
-        try {
-          var iframedoc = iframe.body;
-          if (iframedoc) {
-            // scripts have to be duplicated (maybe this is not needed anymore since using string-templates)
-            var templates = element.contentWindow.parent.document.getElementsByTagName('script');
-            for (var i = 0; i < templates.length; i++) {
-              if (templates[i].getAttribute('type') == 'text/html' && templates[i].getAttribute('id')) {
-                addScriptTemplate(iframe, templates[i].getAttribute('id'), templates[i].innerHTML);
-              }
+                try {
+                    var iframedoc = iframe.body;
+                    if (iframedoc) {
+                        // scripts have to be duplicated (maybe this is not needed anymore since using string-templates)
+                        var templates = element.contentWindow.parent.document.getElementsByTagName('script');
+                        for (var i = 0; i < templates.length; i++) {
+                            if (templates[i].getAttribute('type') == 'text/html' && templates[i].getAttribute('id')) {
+                                addScriptTemplate(iframe, templates[i].getAttribute('id'), templates[i].innerHTML);
+                            }
+                        }
+
+                        var html = iframe.getElementsByTagName("HTML");
+
+                        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+                        ko.cleanNode(html[0] || iframedoc);
+                        });
+
+                        ko.applyBindings(valueAccessor(), html[0] || iframedoc);
+                    } else {
+                    console.log("no iframedoc", local);
+                    }
+                } catch (e) {
+                console.log("error reading iframe.body", e, local);
+                throw e;
+                }
+            } catch (e) {
+            console.log("error reading iframe contentDocument", e, local);
+            throw e;
+            // ignored
             }
-
-            var html = iframe.getElementsByTagName("HTML");
-
-            ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-              ko.cleanNode(html[0] || iframedoc);
-            });
-
-            ko.applyBindings(valueAccessor(), html[0] || iframedoc);
-          } else {
-            console.log("no iframedoc", local);
-          }
+        };
+        try {
+            if (element.contentDocument === null) {
+                element.addEventListener('load', finishBind);
+            } else {
+                finishBind();
+            }
         } catch (e) {
-          console.log("error reading iframe.body", e, local);
-          throw e;
+            console.log("error reading iframe contentDocument", e, local);
+            throw e;
+            // ignored
         }
-      } catch (e) {
-        console.log("error reading iframe contentDocument", e, local);
-        throw e;
-        // ignored
-      }
     }
     bindIframe("first call");
     // older browsers needed this
