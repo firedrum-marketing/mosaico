dcab881a5ad4f417cefd16fba18550cff2c12792
Jonathan Horowitz <jhorowitz@firedrum.com>
2017-04-27 12:58:59 -0700

    Use a unique settings object for each tooltip instance.

--- src/js/bindings/tooltips.js
+++ src/js/bindings/tooltips.js
@@ -8,12 +8,11 @@ var console = require("console");
 
 var extendValueAccessor = function(valueAccessor, obj) {
   return function() {
-    ko.utils.extend(obj, valueAccessor());
-    return obj;
+    return ko.utils.extend(ko.utils.extend({}, obj), valueAccessor());
   };
 };
 
-var options = {
+var defaultOptions = {
   show: {
     delay: 500
   },
@@ -26,12 +25,12 @@ ko.bindingHandlers.tooltips = {
     if (typeof $.fn.tooltip !== 'undefined' && typeof ko.bindingHandlers.tooltip !== 'undefined') {
       // position: { my: "left+15 top+15", at: "center+30 center+30" }
       // NOTE title with "" and " " is needed to avoid default tooltips in native file upload controls
-      return ko.bindingHandlers.tooltip.init(element, extendValueAccessor(valueAccessor, options), allBindingsAccessor, data, context);
+      return ko.bindingHandlers.tooltip.init(element, extendValueAccessor(valueAccessor, defaultOptions), allBindingsAccessor, data, context);
     }
   },
   update: function(element, valueAccessor, allBindingsAccessor, data, context) {
     if (typeof $.fn.tooltip !== 'undefined' && typeof ko.bindingHandlers.tooltip !== 'undefined') {
-      return ko.bindingHandlers.tooltip.update(element, extendValueAccessor(valueAccessor, options), allBindingsAccessor, data, context);
+      return ko.bindingHandlers.tooltip.update(element, extendValueAccessor(valueAccessor, defaultOptions), allBindingsAccessor, data, context);
     }
   },
 };
